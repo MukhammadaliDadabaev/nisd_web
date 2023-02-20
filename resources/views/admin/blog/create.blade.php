c6948df549f5e9b593530219f5193feda518e075
alijalilov <92136722+MukhammadaliDadabaev@users.noreply.github.com>
2023-02-20 09:54:56 +0500

    code refactore

--- resources/views/admin/blog/create.blade.php
+++ resources/views/admin/blog/create.blade.php
@@ -20,7 +20,6 @@
     </div>
     <!-- /.content-header -->
 
-<<<<<<< Updated upstream
     <!-- Main content -->
     <section class="content">
         <div class="container-fluid">
@@ -73,85 +72,11 @@
                             <div class="input-group">
                                 <div class="custom-file">
                                     <input type="file" class="custom-file-input" name="image">
-                                    <label class="custom-file-label">Rasmlarni tanlang</label>
+                                    <label class="custom-file-label">Rasmlarni
+                                        tanlang</label>
                                 </div>
                                 <div class="input-group-append">
                                     <span class="input-group-text">YUKLASH</span>
-=======
-        <!-- Main content -->
-        <section class="content">
-            <div class="container-fluid">
-                <!-- Small boxes (Stat box) -->
-                <div class="row">
-                    <div class="col-12">
-                        <form action="{{ route('admin.blog.store') }}" method="POST" enctype="multipart/form-data">
-                            @csrf
-                            <div class="form-group">
-                                <input type="text" class="form-control" name="title" placeholder="blog nomi"
-                                    value="{{ old('title') }}">
-                                @error('title')
-                                    <div class="text-danger">{{ $message }}</div>
-                                @enderror
-                            </div>
-                            {{-- <div class="form-group">
-                                <input type="text" class="form-control w-25" name="status" placeholder="Status"
-                                    value="{{ old('status') }}">
-                                @error('status')
-                                    <div class="text-danger">{{ $message }}</div>
-                                @enderror
-                            </div> --}}
-                            <div class="form-check mb-3">
-                                <input class="form-check-input" type="checkbox" name="status" checked value="{{ old('status') }}">
-                                <label class="form-check-label">Public</label>
-                                @error('status')
-                                    <div class="text-danger">{{ $message }}</div>
-                                @enderror
-                            </div>
-                            <div class="form-group">
-                                <input type="text" class="form-control" name="lang" placeholder="Til"
-                                    value="{{ old('lang') }}">
-                                @error('lang')
-                                    <div class="text-danger">{{ $message }}</div>
-                                @enderror
-                            </div>
-                            {{-- <div class="form-group">
-                                <input type="text" class="form-control" name="user_id" placeholder="User id"
-                                    value="{{ old('user_id') }}">
-                                @error('user_id')
-                                    <div class="text-danger">{{ $message }}</div>
-                                @enderror
-                            </div> --}}
-                            {{-- <div class="form-group">
-                                <input type="text" class="form-control w-25" name="category_id" placeholder="Category id"
-                                    value="{{ old('lcategory_id') }}">
-                                @error('category_id')
-                                    <div class="text-danger">{{ $message }}</div>
-                                @enderror
-                            </div> --}}
-                            <div class="form-group">
-                                <input type="datetime-local" class="form-control w-25" name="public_date" placeholder="Sana"
-                                    value="{{ old('public_date') }}">
-                                @error('public_date')
-                                    <div class="text-danger">{{ $message }}</div>
-                                @enderror
-                            </div>
-                            <div class="form-group">
-                                <textarea id="summernote" name="content" rows="3" cols="100">{{ old('content') }}</textarea>
-                                @error('content')
-                                    <div class="text-danger">{{ $message }}</div>
-                                @enderror
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputFile">Rasm qo'shing</label>
-                                <div class="input-group">
-                                    <div class="custom-file">
-                                        <input type="file" class="custom-file-input" name="image">
-                                        <label class="custom-file-label">Rasmlarni tanlang</label>
-                                    </div>
-                                    <div class="input-group-append">
-                                        <span class="input-group-text">YUKLASH</span>
-                                    </div>
->>>>>>> Stashed changes
                                 </div>
                             </div>
                             @error('image')
@@ -176,6 +101,7 @@
                         <div class="form-group">
                             <button type="submit" class="btn btn-primary" value="">Qo'shish</button>
                         </div>
+
                     </form>
                 </div>
                 <!-- ./col Название категории -->
